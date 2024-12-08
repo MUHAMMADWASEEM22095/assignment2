02ef9067e0e91b997be1084aedffdb4dff2e8a2d
Waseem <muhammadwaseem22095@gmail.com>
2024-12-08 18:45:27 +0500

    server

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Cards from './Components/Cards';
 import Title from './Components/Title';
 import Footer from './Components/Footer';
 import Complain from './Components/Complain';
+import ComplaintForm from './Components/complaintform';
 function App() {
   return (
-    <div className="App">
-      <Complain/>
-      <Title/>
-      <Cards/>
-      <Footer/>
+    <div className="container my-5">
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header bg-primary text-white">
+            <h2 className="card-title">Complaint Cell</h2>
+          </div>
+          <div className="card-body">
+            <ComplaintForm />
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+
   );
 }
 
